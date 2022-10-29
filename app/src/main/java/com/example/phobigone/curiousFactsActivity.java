3e9504d69f3fbbc3b23f3f9f50096a7d252ebadc
Carolina <carolina.albuquerque.cmp@gmail.com>
2022-10-29 21:57:56 +0100

    Curiosities layout completed on XML file

---
+++ app/src/main/java/com/example/phobigone/curiousFactsActivity.java
@@ -0,0 +1,14 @@
+package com.example.phobigone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class curiousFactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curious_facts);
+    }
+}
