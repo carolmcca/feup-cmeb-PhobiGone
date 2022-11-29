8c03a7a714fb673384e51fe8ecfa1a8b0b3f6a51
marianagandra <102297252+marianagandra@users.noreply.github.com>
2022-11-29 17:32:15 +0000

    results activities and layouts

--- app/src/main/java/com/example/phobigone/TestResultsActivity.java
+++ app/src/main/java/com/example/phobigone/TestResultsActivity.java
@@ -7,37 +7,16 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TrainResultsActivity extends AppCompatActivity {
-<<<<<<< Updated upstream
-=======
+public class TestResultsActivity extends AppCompatActivity {
 
->>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.train_results);
+        setContentView(R.layout.test_results);
 
         Button btHome = findViewById(R.id.homeButton);
         Button btStats = findViewById(R.id.statsButton);
 
-<<<<<<< Updated upstream
-        btHome.setOnClickListener((View v)->onBtClick(btHome.getId()));
-        btStats.setOnClickListener((View v)->onBtClick(btStats.getId()));
-    }
-
-    private void onBtClick(int id) {
-        Intent intent;
-
-        switch (id) {
-            case R.id.homeButton:
-                intent = new Intent(this, MainActivity.class);
-                break;
-            default:
-                intent = new Intent(this, StatsActivity.class);
-        }
-
-        startActivity(intent);
-=======
         btHome.setOnClickListener((View v)->onBtHomeClick());
         btStats.setOnClickListener((View v)->onBtStatsClick());
     }
@@ -46,6 +25,6 @@ public class TrainResultsActivity extends AppCompatActivity {
     }
     private void onBtStatsClick() {
         Intent intent = new Intent(this, StatsActivity.class);
->>>>>>> Stashed changes
     }
+
 }
