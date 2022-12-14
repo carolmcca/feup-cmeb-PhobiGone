818dd4b158966e6df0f48b029af4715d4d0225e2
macdvdup <up201906607@edu.fe.up.pt>
2022-12-14 23:06:41 +0000

    Comment Stats activity
    Still need to check the alteration to SUM

--- app/src/main/java/com/example/phobigone/StatsActivity.java
+++ app/src/main/java/com/example/phobigone/StatsActivity.java
@@ -21,6 +21,7 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class StatsActivity extends AppCompatActivity {
+    // DataBaseHelper Object to access database
     DatabaseHelper dbHelper = new DatabaseHelper(this);
 
     @Override
@@ -28,31 +29,40 @@ public class StatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
 
+        // Get Graph Views from Ids
         GraphView numDailyHoursGraph=findViewById(R.id.hoursGraph);
         GraphView levelEvolutionGraph=findViewById(R.id.levelEvol);
 
-        // Get Views from Ids
+        // Get Spinner View from Ids
         Spinner spinnerOptions = (Spinner) findViewById(R.id.spinnerOptions);
+
         // SET SPINNER OPTIONS
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.options, android.R.layout.simple_spinner_item);
+
         // Specify the layout to use when the list of choices appears
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinnerOptions.setAdapter(spinnerAdapter);
 
+        // Set Listener for Item
         spinnerOptions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                // Get current Settings From Database,
                 HashMap<String, String> settings = dbHelper.getSettings();
+                // more specifically the expected daily training time
                 Integer exp_train_time = Integer.valueOf(settings.get("exp_train_time"));
 
+                // Remove all previous series plotted on the Graph Views
                 numDailyHoursGraph.removeAllSeries();
                 levelEvolutionGraph.removeAllSeries();
 
                 //View chosen option
                 String chosenOption = ((TextView) selectedItemView).getText().toString();
 
+                //Set number of previous days to check
                 Integer numDays=0;
                 switch(chosenOption) {
                     case "Last 30 days":
@@ -63,40 +73,52 @@ public class StatsActivity extends AppCompatActivity {
                         break;
                 }
 
-                String fromdate = getDateToCheckFrom(numDays-1);
+                // Get string of date to check from (format: YYYY/MM/DD)
+                String fromDate = getDateToCheckFrom(numDays-1);
 
-                DataPoint [] trainTime = getLineOfTrainTime(numDays, fromdate);
+                // Get Arrays of DataPoints To Plot the daily train time
+                // and baseline set by the user
+                DataPoint [] trainTime = getLineOfTrainTime(numDays, fromDate);
                 DataPoint [] baseline = getLineOfExpTrainTime(numDays, exp_train_time);
 
+                // Set the Series with the DataPoints
                 LineGraphSeries<DataPoint> trainSeries = new LineGraphSeries<>(trainTime);
                 LineGraphSeries<DataPoint> baselineSeries = new LineGraphSeries<>(baseline);
+                // Set the Series titles and cor of the plot
                 trainSeries.setTitle("Your Trains");
                 baselineSeries.setTitle("Your Goal");
                 trainSeries.setColor(ContextCompat.getColor(getApplicationContext(), R.color.brown));
                 baselineSeries.setColor(ContextCompat.getColor(getApplicationContext(), R.color.darkBrown));
 
+                // Set legend for the GraphView
                 numDailyHoursGraph.getLegendRenderer().setVisible(true);
                 numDailyHoursGraph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
                 numDailyHoursGraph.getLegendRenderer().setMargin(40);
 
+                // Set x-axis according to the days selected and add the Series
                 numDailyHoursGraph.getViewport().setMaxX(numDays + 5);
                 numDailyHoursGraph.getViewport().setMinX(0);
                 numDailyHoursGraph.getViewport().setXAxisBoundsManual(true);
                 numDailyHoursGraph.addSeries(trainSeries);
                 numDailyHoursGraph.addSeries(baselineSeries);
 
-                DataPoint [] level = getLineOfLevel(numDays, fromdate);
+                // Get Arrays of DataPoints To Plot the level evolution throughout
+                DataPoint [] level = getLineOfLevel(numDays, fromDate);
 
+                // Set the respective Series with a title, a color for the plot
+                // and a marker for the data points
                 LineGraphSeries<DataPoint> levelSeries = new LineGraphSeries<>(level);
                 levelSeries.setTitle("Your Level");
                 levelSeries.setColor(ContextCompat.getColor(getApplicationContext(), R.color.brown));
                 levelSeries.setDrawDataPoints(true);
                 levelSeries.setDataPointsRadius(10f);
 
+                // Set legend for the GraphView related to the level evolution
                 levelEvolutionGraph.getLegendRenderer().setVisible(true);
                 levelEvolutionGraph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
                 levelEvolutionGraph.getLegendRenderer().setMargin(40);
 
+                // Set the x and y axis and plot the level evolution Series
                 levelEvolutionGraph.getViewport().setMaxX(numDays + 5);
                 //levelEvolutionGraph.getViewport().setMinX(1);
                 levelEvolutionGraph.getViewport().setXAxisBoundsManual(true);
@@ -106,7 +128,7 @@ public class StatsActivity extends AppCompatActivity {
                 levelEvolutionGraph.addSeries(levelSeries);
             }
 
-            @Override
+            @Override // If nothing is selected nothing is viewed
             public void onNothingSelected(AdapterView<?> parentView) {
                 // your code here
             }
@@ -116,6 +138,8 @@ public class StatsActivity extends AppCompatActivity {
     }
 
     private DataPoint[] getLineOfExpTrainTime(int size, int value) {
+        // this function returns an array of points with the baseline set
+        // for the expected training time
         // size- number of Days
         // value - Expected training time
         DataPoint[] dataPoints = new DataPoint[size];
@@ -126,13 +150,15 @@ public class StatsActivity extends AppCompatActivity {
         return dataPoints;
     }
 
-    private DataPoint[] getLineOfTrainTime(int size, String fromdate) {
+    private DataPoint[] getLineOfTrainTime(int size, String fromDate) {
+        // this function returns an array of points with the daily training Time
+        // from a number of days, dependent on the option selected
         // size- number of Days
-        // fromdate - first day to be considered
+        // fromDate - first day to be considered
         float daily_time;
         DataPoint[] dataPoints = new DataPoint[size];
         for (int point = 0; point < size; point++){
-            List <String> train_time = dbHelper.readRowFromTable("SELECT train_time FROM Train WHERE date='" + fromdate + "'");
+            List <String> train_time = dbHelper.readRowFromTable("SELECT SUM(train_time) FROM Train WHERE date='" + fromDate + "'");
              if (train_time.size() == 0) {
                  daily_time = 0;
              } else {
@@ -141,18 +167,21 @@ public class StatsActivity extends AppCompatActivity {
 
             DataPoint dp = new DataPoint (point+1, daily_time);
             dataPoints[point] = dp;
-            fromdate = getDateToCheckFrom(size - point - 1);
+            fromDate = getDateToCheckFrom(size - point - 1);
         }
         return dataPoints;
     }
 
-    private DataPoint[] getLineOfLevel(int size, String fromdate) {
+    private DataPoint[] getLineOfLevel(int size, String fromDate) {
+        // this function returns an array of points with the level achieved
+        // from a number of days dependent on the option selected
         // size- number of Days
-        // fromdate - first day to be considered
+        // fromDate - first day to be considered
         float daily_level;
         DataPoint[] dataPoints = new DataPoint[size];
         Integer filledPoints = 0;
         for (int point = 0; point < size; point++){
+            // SQL Query gets a list of strings with the average level reached each day
             List <String> test_level = dbHelper.readRowFromTable("SELECT AVG(level) FROM Test WHERE date='" + fromdate + "'");
 
             if (test_level.get(0) != null) {
@@ -161,7 +190,7 @@ public class StatsActivity extends AppCompatActivity {
                 dataPoints[filledPoints] = dp;
                 filledPoints++;
             }
-            fromdate = getDateToCheckFrom(size - point - 2);
+            fromDate = getDateToCheckFrom(size - point - 2);
         }
         DataPoint[] validPoints = new DataPoint[filledPoints];
         System.arraycopy(dataPoints, 0, validPoints, 0, filledPoints);
@@ -169,11 +198,15 @@ public class StatsActivity extends AppCompatActivity {
     }
 
     private String getDateToCheckFrom(int numDays){
+        // Get date in the presented string format
+        // numDays - number of days to present day to check from
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
         Date date = new Date();
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.DATE, -1*numDays);
-        Date todate = cal.getTime();
-        return dateFormat.format(todate);
+        Date toDate = cal.getTime();
+
+        return dateFormat.format(toDate);
     }
 }
